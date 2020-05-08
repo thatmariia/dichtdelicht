b901c3e247616f3fbe3cc1381e9bd5d90dfd11f1
Mariia Turchina <mariia.turchina@gmail.com>
2020-05-09 00:31:56 +0200

    Initial Commit

---
+++ DichtDeLicht/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DichtDeLicht
+//
+//  Created by Mariia Turchina on 09/05/2020.
+//  Copyright © 2020 Mariia Turchina. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
