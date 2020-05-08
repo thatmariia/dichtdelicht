4be66d1c52503ea647b76959e1b3d598030f1b68
Mariia Turchina <mariia.turchina@gmail.com>
2020-05-09 01:11:16 +0200

    fixed .gitignore again lol

--- Pods/Target Support Files/Pods-DichtDeLicht-DichtDeLichtUITests/Pods-DichtDeLicht-DichtDeLichtUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DichtDeLicht_DichtDeLichtUITests : NSObject
-@end
-@implementation PodsDummy_Pods_DichtDeLicht_DichtDeLichtUITests
-@end
--- Pods/Target Support Files/Pods-DichtDeLicht/Pods-DichtDeLicht-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DichtDeLicht : NSObject
-@end
-@implementation PodsDummy_Pods_DichtDeLicht
-@end
