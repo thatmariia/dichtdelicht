78ac47a649ca8e22ca39dcb1fb36ccd5fe1904df
Mariia Turchina <mariia.turchina@gmail.com>
2020-05-09 00:51:50 +0200

    Initial commit

---
+++ DichtDeLicht/DeviceIdentifier.swift
@@ -0,0 +1,15 @@
+//
+//  DeviceIdentifier.swift
+//  DichtDeLicht
+//
+//  Created by Mariia Turchina on 09/05/2020.
+//  Copyright © 2020 Mariia Turchina. All rights reserved.
+//
+
+import Foundation
+
+class DeviceIdentifies {
+    //@Published var device : String
+    
+    
+}
---
+++ Pods/Target Support Files/Pods-DichtDeLichtTests/Pods-DichtDeLichtTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DichtDeLichtTests : NSObject
+@end
+@implementation PodsDummy_Pods_DichtDeLichtTests
+@end
